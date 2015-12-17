1bf7905519ba623b6009cb9bdf5e99cfd55f92ec
Che-Liang Chiou <clchiou@siftscience.com>
2015-12-16 16:01:58 -0800

    Gather more device properties and create persistent device ID

--- Sift/SFDevicePropertiesReporter.m
+++ Sift/SFDevicePropertiesReporter.m
@@ -1,8 +1,12 @@
 // Copyright (c) 2015 Sift Science. All rights reserved.
 
-@import CoreLocation;
+@import AdSupport;
+@import CoreTelephony;
 @import Foundation;
 @import UIKit;
+
+#include <sys/types.h>
+#include <sys/sysctl.h>
 
 #import "SFDebug.h"
 #import "SFUtils.h"
@@ -20,22 +24,180 @@ const SFQueueConfig SFDevicePropertiesReporterQueueConfig = {
 
 @implementation SFDevicePropertiesReporter
 
+static NSString *SFSysctlReadString(const char *name) {
+    int err;
+    size_t size;
+    err = sysctlbyname(name, NULL, &size, NULL, 0);
+    if (err) {
+        SF_DEBUG(@"sysctlbyname(\"%s\", ...): %s", name, strerror(errno));
+        return nil;
+    }
+
+    NSString *value = nil;
+    char buffer[64];
+    void *buf = size < sizeof(buffer) ? buffer : malloc(size);
+    err = sysctlbyname(name, buf, &size, NULL, 0);
+    if (err) {
+        SF_DEBUG(@"sysctlbyname(\"%s\", ...): %s", name, strerror(errno));
+    } else {
+        value = [NSString stringWithCString:buf encoding:NSUTF8StringEncoding];
+    }
+    if (buf != buffer) {
+        free(buf);
+    }
+    return value;
+}
+
+static NSString *SFSysctlReadInt32(const char *name) {
+    int32_t value;
+    size_t size = sizeof(value);
+    int err = sysctlbyname(name, &value, &size, NULL, 0);
+    if (err) {
+        SF_DEBUG(@"sysctlbyname(\"%s\", ...): %s", name, strerror(errno));
+        return nil;
+    } else {
+        return [NSString stringWithFormat:@"%d", value];
+    }
+}
+
+static NSString *SFSysctlReadInt64(const char *name) {
+    int64_t value;
+    size_t size = sizeof(value);
+    int err = sysctlbyname(name, &value, &size, NULL, 0);
+    if (err) {
+        SF_DEBUG(@"sysctlbyname(\"%s\", ...): %s", name, strerror(errno));
+        return nil;
+    } else {
+        return [NSString stringWithFormat:@"%lld", value];
+    }
+}
+
 - (NSDictionary *)createReport {
     NSMutableDictionary *report = [NSMutableDictionary new];
 
     UIDevice *device = [UIDevice currentDevice];
-    [report setObject:[[device identifierForVendor] UUIDString] forKey:@"identifier_for_vendor"];
     for (NSString *propertyName in @[@"name", @"systemName", @"systemVersion", @"model", @"localizedModel"]) {
         SEL selector = NSSelectorFromString(propertyName);
         NSString *(*func)(id, SEL) = (void *)[device methodForSelector:selector];
         NSString *property = [NSString stringWithString:func(device, selector)];
-        [report setObject:property forKey:SFCamelCaseToSnakeCase(propertyName)];
+        if (property) {
+            [report setObject:property forKey:SFCamelCaseToSnakeCase(propertyName)];
+        }
     }
 
-    // TODO(clchiou): Gather more properties...
+    UIScreen *screen = [UIScreen mainScreen];
+    NSInteger width = screen.fixedCoordinateSpace.bounds.size.width * screen.scale;
+    NSInteger height = screen.fixedCoordinateSpace.bounds.size.height * screen.scale;
+    [report setObject:[@(width) stringValue] forKey:@"screen_width"];
+    [report setObject:[@(height) stringValue] forKey:@"screen_height"];
+
+    ASIdentifierManager *identifierManager = [ASIdentifierManager sharedManager];
+    [report setObject:[identifierManager.advertisingIdentifier UUIDString] forKey:@"apple_ifa"];
+    [report setObject:[[device identifierForVendor] UUIDString] forKey:@"apple_ifv"];
+
+    [report setObject:[self getPersistentUniqueId] forKey:@"device_id"];
+
+    CTTelephonyNetworkInfo *networkInfo = [CTTelephonyNetworkInfo new];
+    CTCarrier *carrier = [networkInfo subscriberCellularProvider];
+    if (carrier) {
+        for (NSString *propertyName in @[@"carrierName", @"isoCountryCode", @"mobileCountryCode", @"mobileNetworkCode"]) {
+            SEL selector = NSSelectorFromString(propertyName);
+            NSString *(*func)(id, SEL) = (void *)[carrier methodForSelector:selector];
+            NSString *property = [NSString stringWithString:func(carrier, selector)];
+            if (property) {
+                [report setObject:property forKey:SFCamelCaseToSnakeCase(propertyName)];
+            }
+        }
+    }
+
+    struct {
+        const char *name;
+        NSString *(*read)(const char *);
+    } sysctlProperties[] = {
+        {"hw.machine", SFSysctlReadString},
+        {"hw.model", SFSysctlReadString},
+        {"kern.bootsessionuuid", SFSysctlReadString},
+        {"kern.bootsignature", SFSysctlReadString},
+        {"kern.hostname", SFSysctlReadString},
+        {"kern.ostype", SFSysctlReadString},
+        {"kern.osrelease", SFSysctlReadString},
+        {"kern.uuid", SFSysctlReadString},
+        {"kern.version", SFSysctlReadString},
+
+        {"hw.ncpu", SFSysctlReadInt32},
+        {"hw.byteorder", SFSysctlReadInt32},
+        {"hw.activecpu", SFSysctlReadInt32},
+        {"hw.physicalcpu", SFSysctlReadInt32},
+        {"hw.physicalcpu_max", SFSysctlReadInt32},
+        {"hw.logicalcpu", SFSysctlReadInt32},
+        {"hw.logicalcpu_max", SFSysctlReadInt32},
+        {"hw.cputype", SFSysctlReadInt32},
+        {"hw.cpusubtype", SFSysctlReadInt32},
+        {"hw.cpu64bit_capable", SFSysctlReadInt32},
+        {"hw.cpufamily", SFSysctlReadInt32},
+        {"hw.packages", SFSysctlReadInt32},
+        {"hw.optional.floatingpoint", SFSysctlReadInt32},
+        {"kern.hostid", SFSysctlReadInt32},
+        {"kern.osrevision", SFSysctlReadInt32},
+        {"kern.posix1version", SFSysctlReadInt32},
+        {"user.posix2_version", SFSysctlReadInt32},
+
+        {"hw.busfrequency", SFSysctlReadInt64},
+        {"hw.busfrequency_min", SFSysctlReadInt64},
+        {"hw.busfrequency_max", SFSysctlReadInt64},
+        {"hw.cachelinesize", SFSysctlReadInt64},
+        {"hw.cpufrequency", SFSysctlReadInt64},
+        {"hw.cpufrequency_max", SFSysctlReadInt64},
+        {"hw.cpufrequency_min", SFSysctlReadInt64},
+        {"hw.l1icachesize", SFSysctlReadInt64},
+        {"hw.l1dcachesize", SFSysctlReadInt64},
+        {"hw.l2cachesize", SFSysctlReadInt64},
+        {"hw.l3cachesize", SFSysctlReadInt64},
+        {"hw.memsize", SFSysctlReadInt64},
+        {"hw.pagesize", SFSysctlReadInt64},
+        {"hw.tbfrequency", SFSysctlReadInt64},
+    };
+    for (int i = 0; i < sizeof(sysctlProperties) / sizeof(sysctlProperties[0]); i++) {
+        NSString *property = sysctlProperties[i].read(sysctlProperties[i].name);
+        if (property) {
+            NSString *key = [NSString stringWithCString:sysctlProperties[i].name encoding:NSASCIIStringEncoding];
+            [report setObject:property forKey:key];
+        }
+    }
 
     SF_DEBUG(@"Device properties: %@", report);
     return report;
+}
+
+- (NSString *)getPersistentUniqueId {
+    NSMutableDictionary *keychainItemBase = [NSMutableDictionary new];
+    keychainItemBase[(__bridge id)kSecClass] = (__bridge id)kSecClassGenericPassword;
+    keychainItemBase[(__bridge id)kSecAttrAccessible] = (__bridge id)kSecAttrAccessibleAlways;
+    keychainItemBase[(__bridge id)kSecAttrAccount] = @"device_id";
+    keychainItemBase[(__bridge id)kSecAttrService] = @"siftscience.keychain";
+
+    NSMutableDictionary *keychainItem;
+
+    keychainItem = [NSMutableDictionary dictionaryWithDictionary:keychainItemBase];
+    keychainItem[(__bridge id)kSecReturnAttributes] = (__bridge id)kCFBooleanTrue;
+    keychainItem[(__bridge id)kSecReturnData] = (__bridge id)kCFBooleanTrue;
+    CFDictionaryRef dict = nil;
+    if (SecItemCopyMatching((__bridge CFDictionaryRef)keychainItem, (CFTypeRef *)&dict) == noErr) {
+        NSDictionary *item = (__bridge_transfer NSDictionary *)dict;
+        NSData *data = item[(__bridge id)kSecValueData];
+        NSString *persistentUniqueId = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        SF_DEBUG(@"Return persistent unique ID: %@", persistentUniqueId);
+        return persistentUniqueId;
+    }
+
+    NSString *randomUuid = [[NSUUID UUID] UUIDString];
+    SF_DEBUG(@"Create new persistent unique ID: %@", randomUuid);
+
+    keychainItem = [NSMutableDictionary dictionaryWithDictionary:keychainItemBase];
+    keychainItem[(__bridge id)kSecValueData] = [randomUuid dataUsingEncoding:NSUTF8StringEncoding];
+    SecItemAdd((__bridge CFDictionaryRef)keychainItem, NULL);
+
+    return randomUuid;
 }
 
 - (void)report {

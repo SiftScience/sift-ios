628a1f1b10beadd1a2a803c163541b6adb514fa7
Che-Liang Chiou <clchiou@siftscience.com>
2016-01-04 12:06:05 -0800

    Tune framework project setting

--- Sift/SFDebug.h
+++ Sift/SFDebug.h
@@ -3,8 +3,8 @@
 @import Foundation;
 
 /** Debug output that can be turned on/off by NDEBUG. */
-#ifdef NDEBUG
-#define SF_DEBUG(...)
-#else
+#ifdef DEBUG
 #define SF_DEBUG(FORMAT, ...) NSLog(@"%s:%d " FORMAT, __FUNCTION__, __LINE__, ## __VA_ARGS__)
+#else
+#define SF_DEBUG(...)
 #endif

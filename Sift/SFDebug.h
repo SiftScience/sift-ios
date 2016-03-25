335acf31d9778e1f3923d08888a9e5956978e0fb
Che-Liang Chiou <clchiou@gmail.com>
2016-03-25 16:40:01 -0700

    Disable debug output even in debug builds
    While the debug output was turned off in release build, SDK users who
    are testing the SDK might run the SDK in debug mode and our debug output
    is cluttering the log output.  So we should turn off the debug output in
    debug build as well, and it should be turned on only if asked for.
    
    To turn on the SDK debug output, you should define `SF_DEBUG_ENABLE`
    macro in your build settings.

--- Sift/SFDebug.h
+++ Sift/SFDebug.h
@@ -2,8 +2,8 @@
 
 @import Foundation;
 
-/** Debug output that can be turned on/off by NDEBUG. */
-#ifdef DEBUG
+/** Debug output that can be turned on/off by SF_DEBUG_ENABLE macro. */
+#ifdef SF_DEBUG_ENABLE
 #define SF_DEBUG(FORMAT, ...) NSLog(@"%s:%d " FORMAT, __FUNCTION__, __LINE__, ## __VA_ARGS__)
 #else
 #define SF_DEBUG(...)

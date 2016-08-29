17255ee04ff274140685d691e39932e94513c359
Che-Liang Chiou <clchiou@siftscience.com>
2016-08-29 11:30:59 -0700

    Capture device battery, orientation, and proximity state

--- Sift/SFDevicePropertiesReporter.m
+++ Sift/SFDevicePropertiesReporter.m
@@ -82,6 +82,15 @@ static const int64_t SF_LEEWAY = 5 * NSEC_PER_SEC;
         SFDevicePropertiesReporter * __weak weakSelf = self;
         dispatch_source_set_event_handler(_source, ^{[weakSelf report];});
         dispatch_resume(_source);
+
+        // If developer enables monitoring batter, orientation, and/or proximity sensor, we will receive those notification too.
+        NSNotificationCenter *notification = [NSNotificationCenter defaultCenter];
+        [notification addObserver:self selector:@selector(batteryLevelChanged:) name:UIDeviceBatteryLevelDidChangeNotification object:nil];
+        [notification addObserver:self selector:@selector(batteryStateChanged:) name:UIDeviceBatteryStateDidChangeNotification object:nil];
+        if (UIDevice.currentDevice.generatesDeviceOrientationNotifications) {
+            [notification addObserver:self selector:@selector(orientationChanged:) name:UIDeviceOrientationDidChangeNotification object:nil];
+        }
+        [notification addObserver:self selector:@selector(proximityChanged:) name:UIDeviceProximityStateDidChangeNotification object:nil];
     }
     return self;
 }
@@ -99,6 +108,58 @@ static const int64_t SF_LEEWAY = 5 * NSEC_PER_SEC;
     SFEvent *event = [SFEvent new];
     event.iosDeviceProperties = iosDeviceProperties;
     [sift appendEvent:event withLocation:NO toQueue:SFDevicePropertiesReporterQueueIdentifier];
+}
+
+#pragma mark - Monitor device property changes.
+
+- (void)createEvent:(NSString *)name value:(id)value {
+    SFIosDeviceProperties *properties = [SFIosDeviceProperties new];
+    SFEvent *event = [SFEvent new];
+    event.iosDeviceProperties = properties;
+    [properties setProperty:name value:value];
+    SF_DEBUG(@"Device property: %@ = %@", name, [properties.properties objectForKey:name]);
+    [[Sift sharedInstance] appendEvent:event];
+}
+
+- (void)batteryLevelChanged:(NSNotification *)note {
+    double level = UIDevice.currentDevice.batteryLevel;
+    if (level < 0) {
+        return;
+    }
+    [self createEvent:@"battery_level" value:[NSNumber numberWithDouble:level]];
+}
+
+- (void)batteryStateChanged:(NSNotification *)note {
+    NSString *value = nil;
+    switch (UIDevice.currentDevice.batteryState) {
+        case UIDeviceBatteryStateUnknown: return;
+#define CASE(enum_value) case enum_value: value = @#enum_value; break;
+        CASE(UIDeviceBatteryStateUnplugged)
+        CASE(UIDeviceBatteryStateCharging)
+        CASE(UIDeviceBatteryStateFull)
+#undef CASE
+    }
+    [self createEvent:@"battery_state" value:value];
+}
+
+- (void)orientationChanged:(NSNotification *)note {
+    NSString *value = nil;
+    switch (UIDevice.currentDevice.orientation) {
+        case UIDeviceOrientationUnknown: return;
+#define CASE(enum_value) case enum_value: value = @#enum_value; break;
+        CASE(UIDeviceOrientationPortrait)
+        CASE(UIDeviceOrientationPortraitUpsideDown)
+        CASE(UIDeviceOrientationLandscapeLeft)
+        CASE(UIDeviceOrientationLandscapeRight)
+        CASE(UIDeviceOrientationFaceUp)
+        CASE(UIDeviceOrientationFaceDown)
+#undef CASE
+    }
+    [self createEvent:@"device_orientation" value:value];
+}
+
+- (void)proximityChanged:(NSNotification *)note {
+    [self createEvent:@"proximity_state" value:[NSNumber numberWithBool:UIDevice.currentDevice.proximityState]];
 }
 
 #pragma mark - Device properties.

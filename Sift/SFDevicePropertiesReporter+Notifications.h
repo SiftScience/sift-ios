9de1bbd7afe2b018050bc669ad671972330846ed
Che-Liang Chiou <clchiou@siftscience.com>
2016-08-31 15:32:36 -0700

    Factour out and check application state

---
+++ Sift/SFDevicePropertiesReporter+Notifications.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2016 Sift Science. All rights reserved.
+
+@import Foundation;
+
+/** Receive notifications and report device properties. */
+@interface SFDevicePropertiesReporter (Notificatons)
+
+- (void)registerObservers;
+
+@end

8ea2eec51ff29e24799c6bcd3473e5c898509519
garylee1 <gary@siftscience.com>
2017-01-20 11:06:03 -0800

    v0.9.2 release protocol (#46)

--- Sift.podspec
+++ Sift.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
   spec.name = 'Sift'
-  spec.version = '0.9.1'
+  spec.version = '0.9.2'
   spec.authors = 'Sift Science'
   spec.license = {
     :type => 'MIT',
--- Sift/Sift.m
+++ Sift/Sift.m
@@ -56,7 +56,7 @@ static const SFQueueConfig SFDefaultEventQueueConfig = {
 - (instancetype)initWithRootDirPath:(NSString *)rootDirPath {
     self = [super init];
     if (self) {
-        _sdkVersion = @"v0.9.1";
+        _sdkVersion = @"v0.9.2";
 
         _rootDirPath = rootDirPath;
 

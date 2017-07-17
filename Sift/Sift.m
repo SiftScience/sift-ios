242325d139ffb9cd31e675d0a08a045bd926a0d0
Nitin Pasari <nitinpasari@gmail.com>
2017-07-17 16:18:52 -0700

    Release v0.9.8 (#67)

--- Sift/Sift.m
+++ Sift/Sift.m
@@ -55,7 +55,7 @@ static const SFQueueConfig SFDefaultEventQueueConfig = {
 - (instancetype)initWithRootDirPath:(NSString *)rootDirPath {
     self = [super init];
     if (self) {
-        _sdkVersion = @"v0.9.7";
+        _sdkVersion = @"v0.9.8";
 
         _rootDirPath = rootDirPath;
 

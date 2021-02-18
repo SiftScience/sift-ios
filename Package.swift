53fc2dac8e8c7cbf6c6e4d0157a0e499efca8a36
anjusr <anjusr5@gmail.com>
2021-02-18 12:20:02 +0530

    Renamed Package name

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.-
         .library(
-            name: "sift-ios",
+            name: "sift_ios",
             targets: ["sift-ios"]),
     ],
     dependencies: [

f3a1ddaaeee6b5f9e1cb753c5bbe9b8a16300eba
Michael Thole <michael.thole@doordash.com>
2021-04-26 14:15:56 -0700

    Fix the SPM Package.swift; minor reorganization of the headers to help

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "sift-ios",
+    name: "Sift",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.-
         .library(
-            name: "sift-ios",
-            targets: ["Sift", "sift-ios"]),
+            name: "Sift",
+            targets: ["Sift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,16 +19,16 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "sift-ios",
-            dependencies: ["Sift"]),
-        .target(
             name: "Sift",
             dependencies: [],
             path: "Sift",
-            exclude: ["Info.plist", "README.md"],
-            publicHeadersPath: "."),
+            exclude: ["Info.plist", "README.md"]),
         .testTarget(
-            name: "sift-iosTests",
-            dependencies: ["sift-ios"]),
+            name: "SiftTests",
+            dependencies: ["Sift"],
+            path: "SiftTests",
+            cSettings: [
+                .headerSearchPath("../Sift"),
+            ]),
     ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import sift_iosTests
-
-var tests = [XCTestCaseEntry]()
-tests += sift_iosTests.allTests()
-XCTMain(tests)

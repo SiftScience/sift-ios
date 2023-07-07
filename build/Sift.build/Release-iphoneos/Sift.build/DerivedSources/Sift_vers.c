6ae0dd00cb62f75efd4c6e17ca2b2eb6e4ee9ced
Keith Smiley <keithbsmiley@gmail.com>
2023-07-07 10:32:01 -0700

    Remove checked in build directories

--- build/Sift.build/Release-iphoneos/Sift.build/DerivedSources/Sift_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char SiftVersionString[];
- extern const double SiftVersionNumber;
-
- const unsigned char SiftVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Sift  PROJECT:Sift-1" "\n";
- const double SiftVersionNumber __attribute__ ((used)) = (double)1.;

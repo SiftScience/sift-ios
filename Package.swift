7c3d7b1549c5c9727559f2ff6082ec46eaed2260
Daniel Barden <daniel.barden@gmail.com>
2024-04-08 15:17:46 -0300

    Adds the Privacy Manifest to the list of processed files

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -26,6 +26,9 @@ let package = Package(
             dependencies: [],
             path: "Sift",
             exclude: ["Info.plist", "README.md"],
+            resources: [
+                .process("../PrivacyInfo.xcprivacy")
+            ],
             publicHeadersPath: "."),
         .testTarget(
             name: "sift-iosTests",

c838cd176ef72af2c50b1de670df67cc9ee1eef4
Ihor Prysiazhnyi <iprysiazhnyi@siftscience.com>
2024-06-26 11:33:40 +0300

    Release v2.1.9

--- Sift/Sift.m
+++ Sift/Sift.m
@@ -57,7 +57,7 @@ static const SiftQueueConfig SFDefaultEventQueueConfig = {
 - (instancetype)initWithRootDirPath:(NSString *)rootDirPath {
     self = [super init];
     if (self) {
-        _sdkVersion = @"v2.1.8";
+        _sdkVersion = @"v2.1.9";
 
         _rootDirPath = rootDirPath;
 

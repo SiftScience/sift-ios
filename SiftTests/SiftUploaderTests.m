bae733d8e730012fe746c3908149557d98e67277
Ihor Prysiazhnyi <iprysiazhnyi@siftscience.com>
2024-07-02 15:30:51 +0300

    Improves retries backoff for network errors

--- SiftTests/SiftUploaderTests.m
+++ SiftTests/SiftUploaderTests.m
@@ -107,7 +107,7 @@
         [expectation fulfill];
     };
 
-    int rejectedLimitCount = 3;
+    int rejectedLimitCount = 60;
     for (int i = 0; i < rejectedLimitCount; i++) {
         // Simulate a network error response (status code 1 hardcoded for network errors).
         [stub.stubbedStatusCodes addObject:@1];
@@ -130,7 +130,7 @@
         [expectation fulfill];
     };
     
-    int rejectedLimitCount = 4;
+    int rejectedLimitCount = 61;
     for (int i = 0; i < rejectedLimitCount; i++) {
         // Simulate a network error response (status code 1 hardcoded for network errors).
         [stub.stubbedStatusCodes addObject:@1];
@@ -142,13 +142,76 @@
     XCTWaiter *waiter = [[XCTWaiter alloc] init];
     XCTWaiterResult result = [waiter waitForExpectations:@[expectation] timeout:3.0];
     
-    // The waiter should time out because it's waiting for the final call (call 4) to be made.
-    // However, the call is not made because the system stops retrying after three failed attempts (SF_REJECT_LIMIT).
+    // The waiter should time out because it's waiting for the final call (call 61) to be made.
+    // However, the call is not made because the system stops retrying after 60 failed attempts (SF_DEFAULT_NETWORK_MAX_RETRIES).
     XCTAssertEqual(result, XCTWaiterResultTimedOut);
-    // Assert that no more than 3(SF_REJECT_LIMIT) calls were made
-    XCTAssertEqual(stub.capturedRequests.count, 3);
+    // Assert that no more than 60(SF_DEFAULT_NETWORK_MAX_RETRIES) calls were made
+    XCTAssertEqual(stub.capturedRequests.count, 60);
     // 1 stub should still be in the queue
     XCTAssertEqual(stub.stubbedStatusCodes.count, 1);
+}
+
+- (void)testUploadNetworkErrorsMaxRetriesNotReached {
+    SFHttpStub *stub = [SFHttpStub sharedInstance];
+
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Wait for upload tasks completion"];
+    stub.completionHandler = ^{
+        [expectation fulfill];
+    };
+    
+    int rejectedLimitCount = 59;
+    for (int i = 0; i < rejectedLimitCount; i++) {
+        // Simulate a network error response (status code 1 hardcoded for network errors).
+        [stub.stubbedStatusCodes addObject:@1];
+    }
+    
+    int successfullRequests = 1;
+    for (int i = 0; i < successfullRequests; i++) {
+        [stub.stubbedStatusCodes addObject:@200];
+    }
+    
+    NSArray *events = @[[SiftEvent eventWithType:nil path:@"path" fields:nil]];
+    [_uploader upload:events];
+    
+    [self waitForExpectationsWithTimeout:3.0 handler:nil];
+
+    XCTAssertEqual(stub.capturedRequests.count, rejectedLimitCount + successfullRequests);
+    XCTAssertEqual(stub.stubbedStatusCodes.count, 0);
+}
+
+- (void)testUploadNetworkAndHttpErrorsMaxRetriesNotReached {
+    SFHttpStub *stub = [SFHttpStub sharedInstance];
+
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Wait for upload tasks completion"];
+    stub.completionHandler = ^{
+        [expectation fulfill];
+    };
+    
+    int rejectedLimitCount = 59;
+    for (int i = 0; i < rejectedLimitCount; i++) {
+        // Simulate a network error response (status code 1 hardcoded for network errors).
+        [stub.stubbedStatusCodes addObject:@1];
+    }
+    
+    int rejectedHttpErrorLimitCount = 2;
+    for (int i = 0; i < rejectedHttpErrorLimitCount; i++) {
+        [stub.stubbedStatusCodes addObject:@500];
+    }
+    
+    int successfullRequests = 1;
+    for (int i = 0; i < successfullRequests; i++) {
+        [stub.stubbedStatusCodes addObject:@200];
+    }
+    
+    NSArray *events = @[[SiftEvent eventWithType:nil path:@"path" fields:nil]];
+    [_uploader upload:events];
+    
+    [self waitForExpectationsWithTimeout:3.0 handler:nil];
+
+    
+    XCTAssertEqual(stub.capturedRequests.count, rejectedLimitCount + successfullRequests + rejectedHttpErrorLimitCount);
+    // verify that max retries for network and http errors are not reached
+    XCTAssertEqual(stub.stubbedStatusCodes.count, 0);
 }
 
 @end
